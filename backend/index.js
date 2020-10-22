556ff50123f041e8a3ce8de9b309403f604b2f2a
Guilherme Azevedo <guiazetri@uninove.edu.br>
2020-10-21 22:16:25 -0300

    Migrations Sistema - 346/349

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,14 @@ const app = require('express')()
 const consing = require('consign')
 const consign = require('consign/lib/consign')
 
+const db = require('./config/db')
+
+app.db = db;
+
 consign()
 .then('./config/middlewares.js')
+.then('./api')
+.then('./config/routes.js')
 .into(app)
 
 
