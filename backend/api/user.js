9ec7e618a9011a55b5abe3f536d1804a6629bdf5
Guilherme Azevedo <guiazetri@uninove.edu.br>
2020-10-23 00:03:47 -0300

    350-352
    API Usuario post, get

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,7 +1,37 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
     const save = (req, res) => {
         res.send('user save')
+            .catch(err => res.status(500).send(err))
     }
 
-    return { save }
+    const getById = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .where({ id: req.params.id })
+            .whereNull('deletedAt')
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const remove = async (req, res) => {
+        try {
+            const articles = await app.db('articles')
+                .where({ userId: req.params.id })
+            notExistsOrError(articles, 'Usuário possui artigos.')
+
+            const rowsUpdated = await app.db('users')
+                .update({deletedAt: new Date()})
+                .where({ id: req.params.id })
+            existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
+
+            res.status(204).send()
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getById, remove }
 }
--- backend/api/validation.js
+++ backend/api/validation.js
@@ -1,22 +1,22 @@
 module.exports = app => {
-    function existOrError(value, msg){
+    function existsOrError(value, msg) {
         if(!value) throw msg
         if(Array.isArray(value) && value.length === 0) throw msg
-        if(typeof value === 'string' && !value.trim()) throw msg    
+        if(typeof value === 'string' && !value.trim()) throw msg
     }
     
-    function notExistsOrError(value, msg){
+    function notExistsOrError(value, msg) {
         try {
-            existOrError(value, msg)
-        }catch(msg){
+            existsOrError(value, msg)
+        } catch(msg) {
             return
         }
         throw msg
     }
     
-    function equalsOrError(valueA , valueB, msg){
+    function equalsOrError(valueA, valueB, msg) {
         if(valueA !== valueB) throw msg
     }
 
-    return { existOrError, notExistsOrError, equalsOrError }
+    return { existsOrError, notExistsOrError, equalsOrError }
 }
